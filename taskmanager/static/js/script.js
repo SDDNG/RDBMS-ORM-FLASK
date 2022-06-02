a7da3467caed9e3f118236f056235bf38b8a013c
SDDNG <davidangraham@gmail.com>
2022-06-02 14:03:54 +0000

    Add task

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     var sidenav = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(sidenav);
+
+
+  //datepicker initialization
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: {done: "Select"}
+    
   });
+
+  // select initialization
+  var selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
+
+});
