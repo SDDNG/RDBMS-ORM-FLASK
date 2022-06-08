0fe22ebd61779f27771cb41a916c9a5cdb6031ec
SDDNG <davidangraham@gmail.com>
2022-06-08 09:02:19 +0000

    delete task

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -6,6 +6,7 @@ if os.path.exists("env.py"):
 
 
 app = Flask(__name__)
+print(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
